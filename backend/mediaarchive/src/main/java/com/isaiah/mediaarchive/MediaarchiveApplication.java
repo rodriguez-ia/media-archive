10db581ea8d16395b7b3ec68f745554bc24e8246
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2026-05-16 20:11:52 -0700

    Initial project setup.

---
+++ backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/MediaarchiveApplication.java
@@ -0,0 +1,13 @@
+package com.isaiah.mediaarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaarchiveApplication.class, args);
+	}
+
+}
---
+++ backend/mediaarchive/src/test/java/com/isaiah/mediaarchive/MediaarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiah.mediaarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
