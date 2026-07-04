d143e45ba69467dfa75902179346b68a4c62560a
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2026-07-04 14:47:33 -0700

    Remove unnecessary annotations from registration DTO classes

--- backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/model/dto/RegisterResponseDTO.java
+++ backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/model/dto/RegisterResponseDTO.java
@@ -1,15 +1,11 @@
 package com.isaiah.mediaarchive.model.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.UUID;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class RegisterResponseDTO {
 
     private UUID id;
