0fd2b9c7aa93aa20c1a0f55d0ce1b7c28a3c49db
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2026-08-10 12:52:02 -0700

    Remove unused import.

--- backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/model/entity/UserEntity.java
+++ backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/model/entity/UserEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
