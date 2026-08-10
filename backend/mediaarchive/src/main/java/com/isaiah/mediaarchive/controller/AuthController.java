fe8af517f77a61256aff22725d12e690f004a7ba
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2026-08-10 12:48:36 -0700

    Add whitespace in return for readability

--- backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/controller/AuthController.java
+++ backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/controller/AuthController.java
@@ -22,14 +22,26 @@ public class AuthController {
     public ResponseEntity<ApiResponse<RegisterResponseDTO>> register(@Valid @RequestBody RegisterRequestDTO requestDTO) {
         RegisterResponseDTO responseDTO = authService.register(requestDTO);
 
-        return ResponseEntity.status(201).body(ApiResponseFactory.created(responseDTO, "AuthController.register", "User registered successfully"));
+        return ResponseEntity.status(201).body(
+                ApiResponseFactory.created(
+                        responseDTO,
+                        "AuthController.register",
+                        "User registered successfully"
+                )
+        );
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<LoginResponseDTO>> login(@Valid @RequestBody LoginRequestDTO requestDTO) {
         LoginResponseDTO responseDTO = authService.login(requestDTO);
 
-        return ResponseEntity.status(200).body(ApiResponseFactory.success(responseDTO, "AuthController.login", "User login successful"));
+        return ResponseEntity.status(200).body(
+                ApiResponseFactory.success(
+                        responseDTO,
+                        "AuthController.login",
+                        "User login successful"
+                )
+        );
     }
 
     @GetMapping("/test")
