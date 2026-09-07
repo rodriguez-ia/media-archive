f38399fc29679c1953cd5160791da206b085b141
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2026-09-07 13:56:12 -0700

    Include TMDB genres when returning data.

--- backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/mapper/TMDBMapper.java
+++ backend/mediaarchive/src/main/java/com/isaiah/mediaarchive/mapper/TMDBMapper.java
@@ -3,21 +3,54 @@ package com.isaiah.mediaarchive.mapper;
 import com.isaiah.mediaarchive.model.dto.BaseMediaResponseDTO;
 import com.isaiah.mediaarchive.model.dto.TMDBResponseItemDTO;
 import com.isaiah.mediaarchive.model.dto.TMDBSearchResponseDTO;
+import com.isaiah.mediaarchive.model.enums.GenreEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class TMDBMapper {
 
     private static final Logger log = LoggerFactory.getLogger(TMDBMapper.class);
 
-    private final String COVER_IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
-    private final String MOVIE = "movie";
-    private final String TV = "tv";
+    private static final String COVER_IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+    private static final String MOVIE = "movie";
+    private static final String TV = "tv";
+
+    private static final Map<Integer, GenreEnum> GENRE_BY_TMDB_ID = Map.ofEntries(
+            Map.entry(28, GenreEnum.ACTION),
+            Map.entry(12, GenreEnum.ADVENTURE),
+            Map.entry(16, GenreEnum.ANIMATION),
+            Map.entry(35, GenreEnum.COMEDY),
+            Map.entry(80, GenreEnum.CRIME),
+            Map.entry(99, GenreEnum.DOCUMENTARY),
+            Map.entry(18, GenreEnum.DRAMA),
+            Map.entry(10751, GenreEnum.FAMILY),
+            Map.entry(14, GenreEnum.FANTASY),
+            Map.entry(36, GenreEnum.HISTORY),
+            Map.entry(27, GenreEnum.HORROR),
+            Map.entry(10402, GenreEnum.MUSIC),
+            Map.entry(9648, GenreEnum.MYSTERY),
+            Map.entry(10759, GenreEnum.ACTION_ADVENTURE),
+            Map.entry(10762, GenreEnum.KIDS),
+            Map.entry(10763, GenreEnum.NEWS),
+            Map.entry(10764, GenreEnum.REALITY),
+            Map.entry(10765, GenreEnum.SCI_FI_FANTASY),
+            Map.entry(10766, GenreEnum.SOAP),
+            Map.entry(10767, GenreEnum.TALK),
+            Map.entry(10749, GenreEnum.ROMANCE),
+            Map.entry(878, GenreEnum.SCIENCE_FICTION),
+            Map.entry(10770, GenreEnum.TV_MOVIE),
+            Map.entry(53, GenreEnum.THRILLER),
+            Map.entry(10752, GenreEnum.WAR),
+            Map.entry(10768, GenreEnum.WAR_POLITICS),
+            Map.entry(37, GenreEnum.WESTERN)
+    );
 
     public List<BaseMediaResponseDTO> tmdbResponseToBaseMediaResponseDTOList(TMDBSearchResponseDTO tmdbResponse) {
 
@@ -34,7 +67,10 @@ public class TMDBMapper {
                         tmdbResponseItem.getDisplayTitle(),
                         tmdbResponseItem.getOverview(),
                         tmdbResponseItem.getDisplayMediaType(),
-                        null,
+                        tmdbResponseItem.getGenre_ids()
+                                .stream()
+                                .map(id -> GENRE_BY_TMDB_ID.getOrDefault(id, GenreEnum.OTHER))
+                                .collect(Collectors.toSet()),
                         tmdbResponseItem.getDisplayReleaseDate(),
                         tmdbResponseItem.getVote_average(),
                         COVER_IMG_BASE_URL + tmdbResponseItem.getPoster_path()
